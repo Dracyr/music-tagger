0c7dcb9c86fb0606ef5342dfa03bdfc39c1c2f19
Per-Victor Persson <dracyr@gmail.com>
2016-11-19 23:58:01 +0100

    Add reading of extended properties, readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# music-tagger
+
+This is a small command line app that returns the tags (id3 or otherwise) from a audio file.
+It is written in c++ and uses the taglib library, and returns the tags as serialized json on stdout for easy parsing (subject to change).
+
+
+## Todo
+
+* Should be able to Update tags in file
+* Clean up everything
--- src/tagreader.cpp
+++ src/tagreader.cpp
@@ -1,15 +1,9 @@
-#include <iostream>
-#include <iomanip>
-#include <stdio.h>
-#include <string>
-
 #include <taglib.h>
 #include <fileref.h>
 #include <tag.h>
 #include <tpropertymap.h>
 
 #include "../lib/json.hpp"
-
 using json = nlohmann::json;
 
 using namespace std;
@@ -33,34 +27,73 @@ int main (int argc, char *argv[])
 
   if(!f.isNull() && f.tag()) {
     TagLib::Tag *tag = f.tag();
+    TagLib::PropertyMap ext_tags = f.file()->properties();
 
     data["tags"] = {
-      {"title", tag->title().toCString(true) },
-      {"artist", tag->artist().toCString(true) },
-      {"album", tag->album().toCString(true) },
-      {"year", tag->year() },
-      {"comment", tag->comment().toCString(true) },
-      {"genre", tag->genre().toCString(true) },
-      {"track", tag->track() }
+      {"TITLE", tag->title().toCString(true) },
+      {"ARTIST", tag->artist().toCString(true) },
+      {"ALBUM", tag->album().toCString(true) },
+      {"ALBUMARTIST", "" },      
+      {"YEAR", std::to_string(tag->year()) },
+      {"COMMENT", tag->comment().toCString(true) },
+      {"GENRE", tag->genre().toCString(true) },
+      {"TRACK", std::to_string(tag->track()) }
     };
 
-    // TagLib::PropertyMap tags = f.file()->properties();
+    // Extend with property tags, since taglib will sometimes not populate tag()
+    if ((tag->title() == "") && ext_tags.contains("TITLE")) {
+      data["tags"]["TITLE"] = ext_tags["TITLE"][0].toCString(true);
+    }
+    if ((tag->artist() == "") && ext_tags.contains("ARTIST")) {
+      data["tags"]["ARTIST"] = ext_tags["ARTIST"][0].toCString(true);
+    }
+    if ((tag->album() == "") && ext_tags.contains("ALBUM")) {
+      data["tags"]["ALBUM"] = ext_tags["ALBUM"][0].toCString(true);
+    }
+    if (ext_tags.contains("ALBUMARTIST")) {
+      data["tags"]["ALBUMARTIST"] = ext_tags["ALBUMARTIST"][0].toCString(true);
+    }
+    if ((tag->comment() == "") && ext_tags.contains("COMMENT")) {
+      data["tags"]["COMMENT"] = ext_tags["COMMENT"][0].toCString(true);
+    }
+    if ((tag->genre() == "") && ext_tags.contains("GENRE")) {
+      data["tags"]["GENRE"] = ext_tags["GENRE"][0].toCString(true);
+    }
 
-    // unsigned int longest = 0;
-    // for(TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
-    //   if (i->first.size() > longest) {
-    //     longest = i->first.size();
-    //   }
-    // }
+    if ((tag->track() == 0) && ext_tags.contains("TRACK")) {
+      data["tags"]["TRACK"] = ext_tags["TRACK"][0].toCString(true);
+    } else if (tag->track() == 0) {
+      data["tags"]["TRACK"] = "";
+    }
+    if ((tag->year() == 0) && ext_tags.contains("YEAR")) {
+      data["tags"]["YEAR"] = ext_tags["YEAR"][0].toCString(true);
+    } else if (tag->year() == 0) {
+      data["tags"]["YEAR"] = "";
+    }
 
-    // for(TagLib::PropertyMap::ConstIterator i = tags.begin(); i != tags.end(); ++i) {
-    //   for(TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j) {
-        
-    //     data["extended_tags"][i->first.toCString(true)] = {};
-    //     // cout << i->first << endl;
-    //     // cout << left << std::setw(longest) << i->first << " - " << '"' << *j << '"' << endl;
-    //   }
-    // }
+    // Add all of the remaining tags to the extended tag list
+    unsigned int longest = 0;
+    for(TagLib::PropertyMap::ConstIterator i = ext_tags.begin(); i != ext_tags.end(); ++i) {
+      if (i->first.size() > longest) {
+        longest = i->first.size();
+      }
+    }
+
+    for(TagLib::PropertyMap::ConstIterator i = ext_tags.begin(); i != ext_tags.end(); ++i) {
+      for(TagLib::StringList::ConstIterator j = i->second.begin(); j != i->second.end(); ++j) {
+        if (
+            i->first != "TITLE" &&
+            i->first != "ARTIST" &&
+            i->first != "ALBUM" &&
+            i->first != "ALBUMARTIST" &&
+            i->first != "YEAR" &&
+            i->first != "COMMENT" &&
+            i->first != "GENRE" &&
+            i->first != "TRACK") {
+          data["extended_ext_tags"][i->first.toCString(true)] = i->second[0].toCString(true);
+        }
+      }
+    }
   }
 
   json out = {
@@ -71,6 +104,3 @@ int main (int argc, char *argv[])
   cout << out << endl;
   return 0;
 }
-
-
-// cmake ..;make;./tagreader file:///home/dracyr/Music/test.mp3  
